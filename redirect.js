26173743487bdbf40007e642eb456f9d6d2d0f52
mwaddo <56128638+mwaddo@users.noreply.github.com>
2020-04-22 12:06:26 -0400

    added input validation and redirect

---
+++ redirect.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        window.location.href = "mainpage.html";
+    }, 5000);
+  });
--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ function renderQuestion(doc){
     type.textContent = doc.data().type;
     sector.textContent = doc.data().sector;
     jobtitle.textContent = doc.data().jobtitle;
-    rating.textContent = 111;
+    rating.textContent = "-";
 
     tr.appendChild(number);
     tr.appendChild(question);
@@ -49,14 +49,60 @@ db.collection('Questions').get().then((snapshot) => {
 // save data
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    db.collection('Questions').add({
-        question: form.question.value,
-        employer: form.employer.value,
-        type: displayRadioValue(),
-        sector: form.sector.value,
-        jobtitle: form.jobtitle.value
-    });
+
+    var validation = formValidation();
+
+    if(validation == true){
+        db.collection('Questions').add({
+            question: form.question.value,
+            employer: form.employer.value,
+            type: displayRadioValue(),
+            sector: form.sector.value,
+            jobtitle: form.jobtitle.value
+        });
+
+    }
 });
+
+// input validation
+function formValidation(){
+    if(form.question.value == "" || form.question.value.trim().length == 0){
+        alert('Error: question field is empty.');
+        form.question.focus();
+        console.log("false question empty");
+        return false;
+    }
+    else if(form.question.value.length < 10){
+        alert('Error: question field is too short (must be over 10 characters).');
+        form.question.focus();
+        console.log("false");
+        return false;
+    }else if(form.question.value.slice(-1) != "?"){
+        alert("Error: question field must end with a question mark.");
+        form.question.focus();
+        return false;
+    }else if(form.employer.value == "" || form.employer.value.trim().length == 0){
+        alert('Error: employer field is empty.');
+        form.employer.focus();
+        console.log("false employer");
+        return false;
+    }else if(displayRadioValue() == null){
+        alert('Error: must choose type.');
+        console.log("false type");
+        return false;
+    }else if(form.sector.value == "" || form.sector.value.trim().length == 0){
+        alert('Error: sector field is empty.');
+        console.log("false sector");
+        return false;
+    }
+    else if(form.jobtitle.value == "" || form.jobtitle.value.trim().length == 0){
+        alert('Error: job title field is empty.');
+        console.log("false jobtitle");
+        return false;
+    }else{
+        return true;
+    }
+}
 
 function displayRadioValue() { 
     var radios = document.getElementsByName('radios'); 
@@ -69,3 +115,7 @@ function displayRadioValue() {
        
     } 
 }
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+});
